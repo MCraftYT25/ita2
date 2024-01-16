d126b67271865d2489399b598f115e58c375e61a
MCraftYT25 <93701162+MCraftYT25@users.noreply.github.com>
2024-01-16 19:35:36 +0100

    Add files via upload

---
+++ JS/dictionary.js
@@ -0,0 +1,162 @@
+//Słów musi być przynajmniej 10 innaczej będą błędy
+var dictionary1 = {
+    "bianco": "biały",
+    "blu": "niebieski",
+    "verde": "zielony",
+    "viola": "fioletowy",
+    "prugna": "śliwkowy",
+    "rosso": "czerwony",
+    "arancione": "pomarańczowy",
+    "giallo": "żółty",
+    "oro": "złoty",
+    "rosa": "różowy",
+    "grigio": "szary",
+    "argento": "srebrny",
+    "marrone": "brązowy",
+    "nero": "czarny"
+};
+
+var dictionary2 = {
+    "cane": "pies",
+    "gatto": "kot",
+    "coniglio": "królik",
+    "cavallo": "koń",
+    "mucca": "krowa",
+    "pecora": "owca",
+    "maiale": "świnia",
+    "gallina": "kura",
+    "anatra": "kaczka",
+    "pesce": "ryba"
+};
+
+var dictionary3 = {
+    "pane": "chleb",
+    "latte": "mleko",
+    "uova": "jajka",
+    "carne": "mięso",
+    "pesce": "ryba",
+    "frutta": "owoce",
+    "verdura": "warzywa",
+    "riso": "ryż",
+    "pasta": "makaron",
+    "formaggio": "ser",
+    "mela": "jabłko",
+    "banana": "banan",
+    "arancia": "pomarańcza",
+    "limone": "cytryna",
+    "ciliegia": "wiśnia",
+    "fragola": "truskawka",
+    "mora": "jeżyna",
+    "pesca": "brzoskwinia",
+    "pera": "gruszka",
+    "melone": "melon",
+    "anguria": "arbuz",
+    "pomodoro": "pomidor",
+    "cetriolo": "ogórek",
+    "carota": "marchewka",
+    "patata": "ziemniak",
+    "cipolla": "cebula",
+    "aglio": "czosnek",
+    "peperone": "papryka",
+    "zucca": "dynia",
+    "lattuga": "sałata",
+    "spinaci": "szpinak"
+};
+
+var dictionary4 = {
+    "nord": "północ",
+    "sud": "południe",
+    "est": "wschód",
+    "ovest": "zachód",
+    "destra": "prawo",
+    "sinistra": "lewo",
+    "sopra": "na górze",
+    "sotto": "na dole",
+    "grande": "duży2",
+    "stesso": "ten sam2",
+    "bello": "piękny2",
+    "nuovo": "nowy2",
+    "certo": "pewny2",
+    "vero": "prawdziwy2",
+    "vero": "prawdziwy2",
+};
+
+var dictionary5 = {
+    "ciao": "cześć",
+    "grazie": "dziękuję",
+    "prego": "proszę (w odpowiedzi na grazie)",
+    "scusa": "przepraszam",
+    "si": "tak",
+    "no": "nie",
+    "buongiorno": "dzień dobry",
+    "buonasera": "dobry wieczór",
+    "buonanotte": "dobranoc",
+    "per favore": "proszę (prośba)",
+    "mi chiamo": "nazywam się"
+};
+
+var dictionary6 = {
+    "secondo": "sekunda",
+  "minuto": "minuta",
+  "ora": "godzina",
+  "giorno": "dzień",
+  "settimana": "tydzień",
+  "mese": "miesiąc",
+  "anno": "rok",
+  "ieri": "wczoraj",
+  "oggi": "dzisiaj",
+  "domani": "jutro",
+  "mattina": "rano",
+  "pomeriggio": "popołudnie",
+  "sera": "wieczór",
+  "notte": "noc",
+  "lunedì": "poniedziałek",
+  "martedì": "wtorek",
+  "mercoledì": "środa",
+  "giovedì": "czwartek",
+  "venerdì": "piątek",
+  "sabato": "sobota",
+  "domenica": "niedziela",
+  "gennaio": "styczeń",
+  "febbraio": "luty",
+  "marzo": "marzec",
+  "aprile": "kwiecień",
+  "maggio": "maj",
+  "giugno": "czerwiec",
+  "luglio": "lipiec",
+  "agosto": "sierpień",
+  "settembre": "wrzesień",
+  "ottobre": "październik",
+  "novembre": "listopad",
+  "dicembre": "grudzień"
+};
+
+var dictionary7 = {
+    //potrzebne jeszcze kilka innaczej błedy
+    "sole": "słońce",
+  "pioggia": "deszcz",
+  "neve": "śnieg",
+  "nuvola": "chmura",
+  "vento": "wiatr",
+  "tempesta": "burza",
+  "arcobaleno": "tęcza",
+  "caldo": "ciepło",
+  "freddo": "zimno"
+};
+
+var dictionary4555 = {
+    "grande": "duży2",
+    "stesso": "ten sam2",
+    "bello": "piękny2",
+    "nuovo": "nowy2",
+    "certo": "pewny2",
+    "vero": "prawdziwy2",
+    "buono": "dobry2",
+    "italiano": "włoski2",
+    "vecchio": "stary",
+    "ora": "teraz",
+    "qui": "tutaj",
+    "là": "tam",
+    "sempre": "zawsze"
+    // Dodaj więcej słów do słownika
+};
